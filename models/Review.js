421d5a3a87c70b3d2f4da2002701c603ea3fc8f8
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-10-30 20:18:54 -0400

    added the config/connection.js, .env, working on models

---
+++ models/Review.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+    {
+       id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+       },
+       review: {
+        type: DataTypes.STRING,
+        allowNull: true,
+       },
+       rating: {
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        validate: {min: 1, max: 5}
+       }, 
+       user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        }
+       }
+       
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'review',
+    }
+);
+
+module.exports = Review
