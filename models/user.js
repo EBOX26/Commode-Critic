949ae71b3e0a596a9f392614d78693db577739aa
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-10-31 19:49:08 -0400

    updated userRoutes.js with the CREATE new user block. Also commented out the password req there and in the User.js file for now

--- models/user.js
+++ models/user.js
@@ -16,18 +16,19 @@ User.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        name: {
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true, 
-            validate: {
-                isEmail: true,
-            },
-        },
+        // commenting out email since for now may want it to be username and pw only.
+        // email: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     unique: true, 
+        //     validate: {
+        //         isEmail: true,
+        //     },
+        // },
         password: {
             type: DataTypes.STRING, 
             allowNull: false,
