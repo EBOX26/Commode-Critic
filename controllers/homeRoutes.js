e71966a81814b5768932571584884da5de3bbfd0
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-10-31 20:14:33 -0400

    added .gitignore file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
+// since it has the withAuth condition it is preventing users from viewing the homepage. Should we remove this? 
 router.get('/', withAuth, async (req, res) => {
     try {
         const userData = await User.findAll({
