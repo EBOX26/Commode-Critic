0c53365a9f0b86962e49f7f7b45d88af81d87d53
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-11-06 20:20:08 -0500

    updated handlebars to the new page layout.

---
+++ public/js/main.js
@@ -0,0 +1,104 @@
+// Wait for the DOM content to load before executing the code
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to load the login form template
+    function loadLoginForm() {
+        // Get the login container element
+        var loginContainer = document.getElementById("custom-login-container");
+        loginContainer.innerHTML = ""; // Clear the container before adding the form
+
+        // Clone the content of the template using innerHTML
+        var template = document.getElementById("login-template");
+        var clone = document.createElement("div");
+        clone.innerHTML = template.innerHTML;
+        while (clone.firstChild) {
+            loginContainer.appendChild(clone.firstChild);
+        }
+
+        // Show the form using UIkit modal
+        UIkit.modal(loginContainer).show();
+    }
+
+    // Add a click event listener to the "Log In" button
+    var loginButton = document.querySelector(".btn-login");
+    loginButton.addEventListener("click", loadLoginForm);
+});
+
+// JavaScript function to display the review form when the button is clicked
+function showReviewForm() {
+    // Get the review form template content
+    const templateSource = document.getElementById('reviewFormTemplate').innerHTML;
+
+    // Compile the template using Handlebars
+    const template = Handlebars.compile(templateSource);
+
+    // Render the template into the desired container
+    const reviewFormContainer = document.getElementById('reviewFormContainer');
+    reviewFormContainer.innerHTML = template();
+}
+
+// Add an event listener to trigger the showReviewForm function when the button is clicked
+document.getElementById('writeReviewButton').addEventListener('click', showReviewForm);
+
+// Initialize the map when the page loads
+function initMap() {
+    // Create a map centered at your current location
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        const map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: lat, lng: lng },
+            zoom: 15
+        });
+
+        // Create a Places Service object
+        const service = new google.maps.places.PlacesService(map);
+
+        // Declare an array to store markers
+        const markers = [];
+
+        // Define a request to search for nearby bathrooms
+        const request = {
+            location: { lat: lat, lng: lng },
+            radius: 1000, // Adjust the radius as needed
+            type: 'bathroom'
+        };
+
+        // Perform the nearby search
+        service.nearbySearch(request, function (results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (let i = 0; i < results.length; i++) {
+                    const place = results[i];
+                    // Display markers for each nearby bathroom
+                    createMarker(place, map, markers);
+                }
+            }
+        });
+    });
+}
+
+function createMarker(place, map, markers) {
+    const marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        title: place.name
+    });
+
+    // Add a click event listener to the marker
+    marker.addListener('click', function() {
+        // You can display more information here, e.g., an info window
+        const infoWindow = new google.maps.InfoWindow({
+            content: place.name
+            // You can customize the content with additional information
+        });
+
+        // Open the info window when the marker is clicked
+        infoWindow.open(map, marker);
+    });
+
+    // Store the marker in the array
+    markers.push(marker);
+}
+
+// Make the initMap function globally accessible
+window.initMap = initMap;
